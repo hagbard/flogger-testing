3096f38e064ab038254f6ca64d07a7e83a2116bb
David Beaumont <david.beaumont+github@gmail.com>
2023-05-16 22:14:27 +0200

    Docs.

--- junit4/src/test/java/net/goui/flogger/testing/junit4/FloggerTestRuleTest.java
+++ junit4/src/test/java/net/goui/flogger/testing/junit4/FloggerTestRuleTest.java
@@ -10,7 +10,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 
-import static net.goui.flogger.testing.LevelClass.FINE;
+import static net.goui.flogger.testing.LevelClass.*;
 
 @RunWith(JUnit4.class)
 public class FloggerTestRuleTest {
@@ -34,73 +34,19 @@ public class FloggerTestRuleTest {
 
     // @formatter:off
 
-    // --------------------------------------------------------------------------------------------
-    // value of                   : log.level()
-    // expected to be greater than: SEVERE
-    // but was                    : WARNING
-    // --------------------------------------------------------------------------------------------
-    // logs.assertLog(0).levelIsAbove(SEVERE);
+    logs.assertLog(0).isAtLevel(WARNING);
+    logs.assertLog(0).hasCause(RuntimeException.class);
 
-    // --------------------------------------------------------------------------------------------
-    // value of: log.level()
-    // expected: FINE
-    // but was : INFO
-    // --------------------------------------------------------------------------------------------
-    logs.assertLog(1).isAtLevel(FINE);
+    logs.assertLog(1).isAtLevel(INFO);
+    logs.assertLog(1).containsMatch("[Mm]es+age");
 
-    // --------------------------------------------------------------------------------------------
-    // value of                       : log.message()
-    // expected to contain a match for: ot[A-Z]er
-    // but was                        : Message: <long message that will be truncated for debugging>
-    // --------------------------------------------------------------------------------------------
-    // logs.assertLog(1).messageMatches("ot[A-Z]er");
+    logs.assertLog(2).hasMetadata("foo", 123);
 
-    // --------------------------------------------------------------------------------------------
-    // log metadata did not contain entry {foo: 567}
-    // value of           : log.metadata().get("foo")
-    // expected to contain: 567
-    // but was            : [123]
-    // --------------------------------------------------------------------------------------------
-    // logs.assertLog(2).metadataContains("foo", 567);
+    logs.assertThat().everyLog().atOrAboveLevel(WARNING).contains("Warning");
+    logs.assertThat().noLog().belowLevel(WARNING).hasCause(RuntimeException.class);
 
-    // --------------------------------------------------------------------------------------------
-    // value of            : log.cause()
-    // expected instance of: java.lang.RuntimeException
-    // but was             : null
-    // --------------------------------------------------------------------------------------------
-    // logs.assertLog(1).hasCause(RuntimeException.class);
-
-    // --------------------------------------------------------------------------------------------
-    // value of                                       : logs.everyLog().atOrAboveLevel(WARNING)
-    // expected every log message to contain substring: Woot!
-    // but it was not true
-    // failing logs                                   : << LOG SUMMARIES >>
-    // --------------------------------------------------------------------------------------------
-    // logs.assertThat().everyLog().atOrAboveLevel(WARNING).messageContains("Woot!");
-
-    // --------------------------------------------------------------------------------------------
-    // value of                           : logs.noLog().atOrBelowLevel(WARNING)
-    // expected no log cause to be of type: class java.lang.RuntimeException
-    // but it was not true
-    // failing logs                       : << LOG SUMMARIES >>
-    // --------------------------------------------------------------------------------------------
-    // logs.assertThat().noLog().atOrBelowLevel(WARNING).hasCause(RuntimeException.class);
-
-    // --------------------------------------------------------------------------------------------
-    // value of                                            : logs.anyLog()
-    // expected any log message to match regular expression: xxx|yyy
-    // but it was not true
-    // failing logs                                        : << LOG SUMMARIES >>
-    // --------------------------------------------------------------------------------------------
-    // logs.assertThat().anyLog().messageMatches("xxx|yyy");
-
-    // --------------------------------------------------------------------------------------------
-    // value of                            : logs.anyLog()
-    // expected any log metadata to contain: foo=789
-    // but it was not true
-    // failing logs                        : << LOG SUMMARIES >>
-    // --------------------------------------------------------------------------------------------
-    // logs.assertThat().anyLog().metadataContains("foo", 789);
+    logs.assertThat().anyLog().containsMatch("foo|bar");
+    logs.assertThat().anyLog().belowLevel(INFO).hasMetadata("foo", 123);
 
     // @formatter:on
   }
