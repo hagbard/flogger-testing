7c2dfbd3aef4bf7552502ac72d676e4e1e79045d
David Beaumont <david.beaumont+github@gmail.com>
2023-09-19 18:05:32 +0200

    Added missing docs for test fixtures.

--- junit5/src/main/java/net/goui/flogger/testing/junit5/FloggerTestExtension.java
+++ junit5/src/main/java/net/goui/flogger/testing/junit5/FloggerTestExtension.java
@@ -30,32 +30,65 @@ import org.junit.jupiter.api.extension.ExtensionContext;
 
 public final class FloggerTestExtension extends TestingApi<FloggerTestExtension>
     implements BeforeEachCallback, AfterEachCallback {
+  /**
+   * Returns a test fixture for the class-under-test at the specified level. It is assumed that the
+   * class being tested and the test class are named {@code Foo} and {@code FooTest} respectively,
+   * and exist in the same package.
+   */
   public static FloggerTestExtension forClassUnderTest(LevelClass level) {
     Class<?> caller = StackWalker.getInstance(RETAIN_CLASS_REFERENCE).getCallerClass();
     return forClassOrPackage(guessClassUnderTest(caller), level);
   }
 
+  /**
+   * Returns a test fixture for all classes in the package-under-test at the specified level. It is
+   * assumed that the classes being tested and the test class exist in the same package. This will
+   * also capture logs from "sub packages" (i.e. those sharing the same package prefix).
+   */
   public static FloggerTestExtension forPackageUnderTest(LevelClass level) {
     Class<?> caller = StackWalker.getInstance(RETAIN_CLASS_REFERENCE).getCallerClass();
     return forClassOrPackage(guessPackageUnderTest(caller), level);
   }
 
+  /** Returns a test fixture for the given class at the specified level. */
   public static FloggerTestExtension forClass(Class<?> clazz, LevelClass level) {
     return forClassOrPackage(loggerNameOf(clazz), level);
   }
 
+  /**
+   * Returns a test fixture for the given package at the specified level. This will also capture
+   * logs from "sub packages" (i.e. those sharing the same package prefix).
+   */
   public static FloggerTestExtension forPackage(Package pkg, LevelClass level) {
     return forClassOrPackage(pkg.getName(), level);
   }
 
+  /**
+   * Returns a test fixture for the named class or package at the specified level. Note that this is
+   * often a more brittle way to specify what's being captured and can easily break under
+   * refactoring.
+   */
   public static FloggerTestExtension forClassOrPackage(String loggerName, LevelClass level) {
     return forLevelMap(ImmutableMap.of(loggerName, level));
   }
 
+  /**
+   * Returns a test fixture which captures log for all entries in the given map of class/package
+   * names to log level. This is the most general case you normally need to use, and can specify
+   * different log levels for different classes/packages. However, if you find yourself using this
+   * method regularly, it might be a sign that your logging testing is not well encapsulated.
+   *
+   * @param levelMap a map of class/package names to the corresponding log level which is to be
+   *     captured for testing.
+   */
   public static FloggerTestExtension forLevelMap(Map<String, LevelClass> levelMap) {
     return create(levelMap, null);
   }
 
+  /**
+   * A special-case method to create a log fixture with a specified log interceptor. In general, you
+   * should not need to use this method.
+   */
   public static FloggerTestExtension create(
       Map<String, LevelClass> levelMap, @Nullable LogInterceptor interceptor) {
     return new FloggerTestExtension(levelMap, interceptor);
